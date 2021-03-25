0ce2e688f8f937a49fed09d4bb38accb431f3ed9
watermelon <laimin@trustkernel.com>
2021-03-25 19:13:15 +0800

    加入bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <b-container fluid>
+            <b-row>
+                <b-col id="nav" cols="3" style="border: solid 1px red">
+                    <b-row>搜索</b-row>
+                    <b-row>全部</b-row>
+                    <b-row>喜欢</b-row>
+                    <b-row>个人中心</b-row>
+                </b-col>
+                <b-col id="content" cols="9">
+                    <router-view/>
+                </b-col>
+            </b-row>
+        </b-container>
     </div>
-    <router-view />
-  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+    }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #nav{
+      min-height: 600px;
+    }
 </style>
